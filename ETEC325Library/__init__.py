c8168e33f0515bdef9ec40a72e0cc392c9bf02b7
User10166 <rduke01@nyit.edu>
2024-12-08 07:32:07 -0500

    Update __init__.py

--- ETEC325Library/__init__.py
+++ ETEC325Library/__init__.py
@@ -1,8 +1,1 @@
-import sys
 
-sys.path.insert(0, './Introduction')
-sys.path.insert(0, './Module01')
-sys.path.insert(0, './Module04')
-sys.path.insert(0, './Module07')
-sys.path.insert(0, './Module12')
-sys.path.insert(0, './Module16')
