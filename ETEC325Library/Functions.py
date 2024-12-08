3cfbb459c1c3a9d893afc1a074c6817a23560372
User10166 <rduke01@nyit.edu>
2024-12-08 07:37:41 -0500

    Update Functions.py

--- ETEC325Library/Functions.py
+++ ETEC325Library/Functions.py
@@ -1,1 +1,184 @@
+import numpy as np
+import pandas as pd
+import sys
+from docx import Document
+from docx.shared import Pt
+from docx.enum.section import WD_ORIENT
+from docx.shared import Inches
+import matplotlib.pyplot as plt
 
+stdoutInstance = sys.stdout
+
+class WordDocument():
+    def __init__(self, file):
+        self.file = file + ".docx";
+        self.document = Document()
+        sections = self.document.sections
+        for section in sections:
+            section.top_margin = Inches(0.5)
+            section.bottom_margin = Inches(0.5)
+            section.left_margin = Inches(0.5)
+            section.right_margin = Inches(0.5)
+            section.orientation = WD_ORIENT.LANDSCAPE
+            new_width, new_height = section.page_height, section.page_width
+            section.page_width = new_width
+            section.page_height = new_height
+        style = self.document.styles['No Spacing']
+        font = style.font
+        font.name = 'Courier New'
+        font.size = Pt(11)
+        self.text = ''
+
+    def writeText(self, text):
+        if(self.text == ''):
+            self.text = text
+        else:
+            self.text = self.text + text
+
+    def saveDocument(self):
+        paragraph = self.document.add_paragraph(self.text)
+        paragraph.style = self.document.styles['No Spacing']
+        self.document.save(self.file)
+
+class Logger(object):
+    def __init__(self, file):
+        self.terminal = sys.stdout
+        self.wordDocument = WordDocument(file)
+   
+    def write(self, message):
+        self.terminal.write(message)
+        self.wordDocument.writeText(message)
+
+    def closeLog(self):
+        self.wordDocument.saveDocument()
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass
+
+    def __del__(self):
+        self.closeLog()
+        sys.stdout = stdoutInstance
+
+def enableLogging(file):
+    sys.stdout = Logger(file)
+
+def disableLogging():
+    sys.stdout.closeLog()
+    sys.stdout = stdoutInstance
+
+def createRange(start, stop, step=1):
+    inclusiveRange = np.arange(start, stop + step, step)
+    rangeShape = inclusiveRange.shape
+    return np.reshape(inclusiveRange, (1, rangeShape[0]))
+    
+def printTable(array, columnNames=None):
+    arrayShape = array.shape
+    if(len(arrayShape) == 1):
+        array = np.reshape(array, (1, arrayShape[0]))
+        arrayShape = array.shape
+    if(columnNames is None):
+        if(len(arrayShape) == 1):
+            numRows = int(arrayShape[0])
+            blankRows = ['' for i in range(numRows)]
+            df = pd.DataFrame(array, index=blankRows, columns=[''])
+        elif(len(arrayShape) > 1):
+            numRows = int(arrayShape[0])
+            blankRows = ['' for i in range(numRows)]
+            numColumns = int(arrayShape[1])
+            blankColumns = ['' for i in range(numColumns)]
+            df = pd.DataFrame(array, index=blankRows, columns=blankColumns)
+    else:
+        numRows = int(arrayShape[0])
+        blankRows = ['' for i in range(numRows)]
+        df = pd.DataFrame(array, index=blankRows, columns=columnNames)
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
+    print(df)
+    
+def printVariable(label, var, columnNames=None):
+    if(type(var) is np.ndarray):
+        print("\n" + label + ":")
+        printTable(var, columnNames)
+    else:
+        print("\n" + label + ": " + str(var))
+
+def readExcelSpreadsheet(file, useColumnNames=True, useRowLabels=False):
+    if(useColumnNames):
+        headerIndex = 0
+    else:
+        headerIndex = None
+
+    if(useRowLabels):
+        rowIndex = 0
+    else:
+        rowIndex = None
+    data = pd.read_excel(file, header=headerIndex, index_col=rowIndex)
+    dataNumbers = data.to_numpy()
+    indexList = list(data.index)
+    namesList = list(data.columns)
+
+    return dataNumbers, indexList, namesList
+
+def plotHistogram(data, bins=None, title=None, xAxisTitle=None, yAxisTitle=None, save=False, saveDir=None):
+    plt.hist(data, bins=bins)
+    if(title is not None):
+        plt.title(title)
+    if(xAxisTitle is not None):
+        plt.xlabel(xAxisTitle)
+    if(yAxisTitle is not None):
+        plt.ylabel(yAxisTitle)
+
+    if(save and saveDir is not None):
+        plt.savefig(saveDir)
+
+    plt.show()
+
+    plt.clf()
+    plt.gcf()
+
+def getMLDataFromExcelSpreadsheet(file, useColumnNames=True, categoriesRow=-1, useNumericCategories=False):
+    if(useColumnNames):
+        headerIndex = 0
+    else:
+        headerIndex = None
+
+    data = pd.read_excel(file, header=headerIndex, index_col=categoriesRow)
+    X = data.to_numpy()
+    Y = None
+    indexList = list(data.index)
+    uniqueIndexesList = list(data.index.unique())
+    numClasses = len(uniqueIndexesList)
+    if(useNumericCategories):
+        Y = [int(idx) for idx in indexList]
+    else:
+        Y = [uniqueIndexesList.index(idx) for idx in indexList]
+    namesList = list(data.columns)
+
+    return X, Y, uniqueIndexesList, numClasses, namesList
+
+def getRawTextMLDataFromExcelSpreadsheet(file, useColumnNames=True, categoriesRow=-1, useNumericCategories=False):
+    if(useColumnNames):
+        headerIndex = 0
+    else:
+        headerIndex = None
+
+    data = pd.read_excel(file, header=headerIndex, index_col=categoriesRow)
+    XRaw = data[data.columns[0]].tolist()
+    XRaw = [str(s) for s in XRaw]
+    XRaw = [s.replace("\\n", "\n") for s in XRaw]
+    XRaw = [s.replace("\\r", "\r") for s in XRaw]
+    XRaw = [s.replace("\\t", "\t") for s in XRaw]
+    Y = None
+    indexList = list(data.index)
+    uniqueIndexesList = list(data.index.unique())
+    numClasses = len(uniqueIndexesList)
+    if(useNumericCategories):
+        Y = [int(idx) for idx in indexList]
+    else:
+        Y = [uniqueIndexesList.index(idx) for idx in indexList]
+    namesList = list(data.columns)
+
+    return XRaw, Y, uniqueIndexesList, numClasses, namesList
