8e06070218b60024862fa821ceeb026e4f8c83ad
User10166 <rduke01@nyit.edu>
2024-12-08 07:40:39 -0500

    Delete ETEC325Library/Module07 directory

--- ETEC325Library/Module07/Functions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def runCentralLimitTheorem(A, sampleSize):
-    sampleMeans = []
-    for i in range(sampleSize):
-        sample = np.random.choice(A, size=len(A), replace=True)
-        sampleMeans.append(np.mean(sample))
-
-    return np.array(sampleMeans)
