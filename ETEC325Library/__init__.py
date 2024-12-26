9f2a79059e9e9a25d163850dd15be0c017948ad6
User10166 <rduke01@nyit.edu>
2024-12-26 17:39:11 -0500

    Update __init__.py

--- ETEC325Library/__init__.py
+++ ETEC325Library/__init__.py
@@ -1,1 +1,2 @@
-
+from .Functions import enableLogging, createRange, printTable, printVariable, readExcelSpreadsheet, plotHistogram, getMLDataFromExcelSpreadsheet, getRawTextMLDataFromExcelSpreadsheet
+from .Algorithms import encodeBacon, decodeBacon, runCentralLimitTheorem
